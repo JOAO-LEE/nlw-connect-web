8e52dad7d669a8b0af01eca623caf2952fd5d15a
João Lee <joaumlimaum@gmail.com>
2025-02-18 10:15:53 -0300

    build: install biome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Button from "@/components/button";
 import IconButton from "@/components/icon-button";
 import InputField, { InputIcon, InputRoot } from "@/components/input";
-// import Input from "@/components/input";
 import { ArrowRight, Copy, Mail } from "lucide-react";
 
 export default function Home() {
